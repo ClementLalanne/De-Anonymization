d08a1da8afac567febb440b3e7d583d8ef5a1d4a
ClementLalanne <clement.lalanne@ens.fr>
2017-06-13 10:12:02 +0200

    Use functors

--- ER_graph.ml
+++ ER_graph.ml
@@ -2,15 +2,17 @@ open Primary_graph
 open Random
 open FPS
 
-let generator n p =
-  (*Generates a Erdos-Renyi graph in G(n,p)*)
-  Random.self_init ();
-  let g = Graph.create n in
-  for i = 0 to (n-1) do
-    for j = 0 to (i-1) do
-      let x = Random.float 1. in
-      if (x < p) then
-        Graph.add_edge g i j
+module ER_graph(Graph : G) = struct
+  let generator n p =
+    (*Generates a Erdos-Renyi graph in G(n,p)*)
+    Random.self_init ();
+    let g = Graph.create n in
+    for i = 0 to (n-1) do
+      for j = 0 to (i-1) do
+        let x = Random.float 1. in
+        if (x < p) then
+          Graph.add_edge g i j
+      done;
     done;
-  done;
-  g
+    g
+end
